63e43b8d1ae745c42d2346dfe175f091ed9bc566
René Monroy <rene.monroy@gmail.com>
2015-06-13 22:50:38 -0500

    Use custom ace component in the app directly for now

--- app/app.jsx
+++ app/app.jsx
@@ -1,17 +1,33 @@
 import React from 'react/addons';
 import Editor from './components/editor';
 
+require('brace/mode/markdown');
+require('brace/theme/github');
+
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.onEditorChange = this.onEditorChange.bind(this);
+  }
+
+  onEditorChange(e) {
+    console.group('Editor Changes');
+    console.log(e);
+    console.groupEnd();
   }
 
   render() {
     return(
       <div {...this.props} className="app">
         <p>App</p>
-        <Editor keyName="poet" />
+        <Editor
+          id="poet-editor"
+          mode="markdown"
+          theme="github"
+          width="100%"
+          onEdit={this.onEditorChange}
+          />
       </div>
     );
   }
