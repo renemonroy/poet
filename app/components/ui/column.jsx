1452ac9bf4cbc5f57fc0cf72e6db5ff2785999eb
René Monroy <rene.monroy@gmail.com>
2015-06-15 02:16:06 -0500

    Add comments for the api of UIColumn

--- app/components/ui/column.jsx
+++ app/components/ui/column.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import InlineStyles from 'react-style';
 import lodash from 'lodash';
 
+/**
+ * Main styles for the UIColumn component. Its visibility relies in
+ * Flex usage because its flexibility to align elements.
+ */
 const inlineStyles = InlineStyles.create({
   ISColumn : {
     flex : 1
   }
 });
 
+/**
+ * Main class for the UIColumn component. This is mainly used by the
+ * UIRow component to show children into columns.
+ */
 export default class UIColumn extends React.Component {
 
+  /**
+   * Initializes the state of the column to change its width but the
+   * width is manipulated by the UIRow in reality.
+   */
   constructor(props) {
     super(props);
     this.state = { colWidth : props.colWidth ? parseInt(props.colWidth) : null };
   }
 
+  /**
+   * Renders a wrapper div that works as a column element. All
+   * children passed on props will be renderend inside this wrapper.
+   */
   render() {
     let ps = this.props,
       st = this.state,
