7cc7853edba7d50d98e48f6f207686170f6724a2
René Monroy <rene.monroy@gmail.com>
2015-06-18 00:42:00 -0500

    Add an automatic with to the editor

--- app/app.jsx
+++ app/app.jsx
@@ -22,23 +22,31 @@ export default class App extends React.Component {
     this.setState({ markdown : e });
   }
 
+  getElWidth(el) {
+    return window.getComputedStyle(React.findDOMNode(el)).width;
+  }
+
   render() {
     let st = this.state,
-      { ISColContainer } = inlineStyles;
+      { ISColContainer } = inlineStyles,
+      editorWidth = this.refs.editorWrapper ?
+        this.getElWidth(this.refs.editorWrapper) : '100%';
     return(
       <div {...this.props} className="app">
         <p>App</p>
         <UIRow id="row-editor" resizable="true">
-          <div style={ISColContainer}>
+          <div ref="editorWrapper" style={ISColContainer}>
             <UIEditor
               id="poet-editor"
               mode="markdown"
               theme="github"
-              width="100%"
+              width={editorWidth}
+              showGutter={false}
+              highlightActiveLine={false}
               onEdit={this.onEditorChange}
             />
           </div>
-          <div style={ISColContainer}>
+          <div ref="articleWrapper" style={ISColContainer}>
             <UIArticle
               id="poet-previewer"
               content={st.markdown}
