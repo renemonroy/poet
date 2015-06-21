a340f1ab30cea41d77a4a53ad4eb7774c5510742
René Monroy <rene.monroy@gmail.com>
2015-06-21 02:09:24 -0500

    Build article with sections and columns

--- app/app.jsx
+++ app/app.jsx
@@ -1,58 +1,32 @@
 import React from 'react';
 import InlineStyles from 'react-style';
-import UIRow from './components/ui/row.jsx';
-import UIEditor from './components/ui/editor';
-import UIArticle from './components/ui/article';
+import Article from './components/article';
 
-const inlineStyles = InlineStyles.create({
-  ISColContainer : {
-    padding : '20px'
-  }
-});
+let getArticleState = function() {
+  return {
+    article : JSON.parse(localStorage.getItem('article'))
+  };
+};
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { markdown : '' };
-    this.onEditorChange = this.onEditorChange.bind(this);
+    this.state = getArticleState();
   }
 
-  onEditorChange(e) {
-    this.setState({ markdown : e });
-  }
-
-  getElWidth(el) {
-    return window.getComputedStyle(React.findDOMNode(el)).width;
+  renderArticle() {
+    let { article } = this.state;
+    return (
+      <Article id={article.id} name={article.name} content={article.content} />
+    );
   }
 
   render() {
-    let st = this.state,
-      { ISColContainer } = inlineStyles,
-      editorWidth = this.refs.editorWrapper ?
-        this.getElWidth(this.refs.editorWrapper) : '100%';
+    let { article } = this.state;
     return(
-      <div {...this.props} className="app">
-        <p>App</p>
-        <UIRow id="row-editor" resizable="true">
-          <div ref="editorWrapper" style={ISColContainer}>
-            <UIEditor
-              id="poet-editor"
-              mode="markdown"
-              theme="github"
-              width={editorWidth}
-              showGutter={false}
-              highlightActiveLine={false}
-              onEdit={this.onEditorChange}
-            />
-          </div>
-          <div ref="articleWrapper" style={ISColContainer}>
-            <UIArticle
-              id="poet-previewer"
-              content={st.markdown}
-            />
-          </div>
-        </UIRow>
+      <div className="app">
+        { article ? this.renderArticle() : null }
       </div>
     );
   }
---
+++ app/components/article.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const displayName = 'Article';
+const defaultProps = {
+  id : null,
+  name : '',
+  content : {}
+};
+
+export default class Article extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  renderColumns(columns) {
+    return columns.map( function(column) {
+      let content = { __html : column.content || '' };
+      return (
+        <div className="column" key={'column' + column.id}>
+          <div className="content" dangerouslySetInnerHTML={content} />
+        </div>
+      );
+    });
+  }
+
+  renderRows(rows) {
+    let article = this;
+    return rows.map( function(row) {
+      return (
+        <div className="row" key={'row' + row.id}>
+          { row.columns ? article.renderColumns(row.columns) : null }
+        </div>
+      );
+    });
+  }
+
+  render() {
+    let { content } = this.props;
+    return (
+      <article>
+        { content.rows ? this.renderRows(content.rows) : null }
+      </article>
+    );
+  }
+
+};
+
+Article.displayName = displayName;
+Article.defaultProps = defaultProps;
